f7f0621298c0c274b8780502e055faab8930c8e1
Jayanthrowthcv <56308365+Jayanthrowthcv@users.noreply.github.com>
2019-11-13 16:57:01 +0530

    Update home.component.ts

--- angular-fe4xcf/src/app/home/home.component.ts
+++ angular-fe4xcf/src/app/home/home.component.ts
@@ -9,23 +9,41 @@ import { HomeService } from "./home.service";
 export class HomeComponent implements OnInit {
   name = "Angular";
   cartMessage = ``;
-  arrayList: Array<Object> = [];
+  arrayList: Array<any> = [];
   buttonEnable = [];
   cartCount = [];
-  
+
   constructor(private service: HomeService) {}
 
   ngOnInit() {
     this.cartCount = this.service.cartDetails;
     this.service.getMethod().subscribe(res => {
       this.arrayList = res;
+      if (this.service.buttonEnable.length == 0) {
+        for (let i = 0; i < this.arrayList.length; i++) {
+          this.buttonEnable[i] = true;
+          this.service.buttonEnable = this.buttonEnable;
+        }
+      } else {
+        this.buttonEnable = this.service.buttonEnable;
+      }
     });
-    for (let i = 0; i < this.arrayList.length; i++) {}
   }
   addToCart(item) {
     console.log(item);
-    let obj = { itemName: item.name, count: 1 ,price : item.price, discount: item.discount ,img :item.img_url};
+    let obj = {
+      itemName: item.name,
+      count: 1,
+      price: item.price,
+      discount: item.discount,
+      img: item.img_url
+    };
     this.cartMessage = `${item.name} is added to cart`;
     this.service.cartDetails.push(obj);
+    this.arrayList.forEach((x, i) => {
+      if (x.id == item.id) {
+        this.buttonEnable[i] = false;
+      }
+    });
   }
 }
